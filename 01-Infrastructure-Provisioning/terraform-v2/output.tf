6cb6deeb34e8220c492310ef627208656a0439fc
Igor Voitov <Igor.Voitov@appxite.com>
2019-02-26 18:53:55 +0300

    fix exposing private key instead of pulic

--- 01-Infrastructure-Provisioning/terraform-v2/output.tf
+++ 01-Infrastructure-Provisioning/terraform-v2/output.tf
@@ -11,5 +11,5 @@ output "public_instance_ip" {
   value = ["${aws_instance.testInstance.public_ip}"]
 }
 output "private_key_pem" {
-  value = ["${tls_private_key.example.public_key_openssh}"]
+  value = ["${tls_private_key.example.private_key_pem}"]
 }
