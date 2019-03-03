5685595c9e4d6fa5d2ddd2f76f75fa4bd20cd053
Igor Voitov <Igor.Voitov@appxite.com>
2019-03-03 17:46:38 +0300

    fixed env vars

--- 01-Infrastructure-Provisioning/main.tf
+++ 01-Infrastructure-Provisioning/main.tf
@@ -1,7 +1,7 @@
-variable "AWS_ACCESS_KEY" {
+variable "access_key" {
   default = "$AWS_ACCESS_KEY"
 }
-variable "AWS_SECRET_KEY" {
+variable "secret_key" {
   default = "$AWS_SECRET_KEY"
 }
 
@@ -9,8 +9,8 @@ variable "AWS_SECRET_KEY" {
 module "terraform-aws-vpc-ec2" {
   source = "./terraform-aws-vpc-ec2"
 
-access_key = "${var.AWS_ACCESS_KEY}"
-secret_key = "${var.AWS_SECRET_KEY}"
+access_key = "${var.access_key}"
+secret_key = "${var.secret_key}"
 
 
 }
--- 01-Infrastructure-Provisioning/terraform-aws-vpc-ec2/variables.tf
+++ 01-Infrastructure-Provisioning/terraform-aws-vpc-ec2/variables.tf
@@ -1,6 +1,6 @@
 # Variables
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "access_key" {}
+variable "secret_key" {}
 variable "region" {
   default = "eu-central-1"
 }
