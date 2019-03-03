abb4da4deff2658c0b6142f8679b6198874516a7
Igor Voitov <Igor.Voitov@appxite.com>
2019-03-03 18:34:56 +0300

    fix

--- 01-Infrastructure-Provisioning/main.tf
+++ 01-Infrastructure-Provisioning/main.tf
@@ -1,5 +1,9 @@
-variable "access" {}
-variable "secret" {}
+variable "access" {
+  default = "mykey"
+}
+variable "secret" {
+  default = "mykey"
+}
 
 module "terraform-aws-vpc-ec2" {
   source = "./terraform-aws-vpc-ec2"
