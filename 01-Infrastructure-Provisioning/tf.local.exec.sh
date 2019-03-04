319e2aa08533c2e21cba7123d909a6dac1f19482
Igor Voitov <Igor.Voitov@appxite.com>
2019-03-04 10:02:26 +0300

    fix tf gitlab provision

--- 01-Infrastructure-Provisioning/tf.local.exec.sh
+++ 01-Infrastructure-Provisioning/tf.local.exec.sh
@@ -1,1 +1,9 @@
-terraform plan -out "planfile" -var 'access=value' -var 'secret=value'
+#pwsh
+terraform plan -out "planfile" -var "access=$env:AWS_ACCESS_KEY" -var "secret=$env:AWS_SECRET_KEY"
+terraform apply "planfile" 
+
+
+#bash
+terraform init
+terraform plan -out "planfile" -var "access=$AWS_ACCESS_KEY" -var "secret=$AWS_SECRET_KEY"
+terraform apply "planfile"
