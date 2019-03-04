b8dfc0da8c2e50ff07d2e7a06031447c09151c38
Igor Voitov <Igor.Voitov@appxite.com>
2019-03-04 08:59:39 +0300

    removed tmp python app

--- 02-Coding-skills/docker.exec.sh
+++
@@ -1,3 +0,0 @@
-docker build --no-cache --rm -f "Dockerfile" -t igorvoitov/02-coding-skills:latest .
-docker build --no-cache --rm -f "Dockerfile" -t devtest01registry01.azurecr.io/02-coding-skills:latest .
-docker run --rm -it -p 5000:5000 igorvoitov/02-coding-skills:latest
--- 03-Kubernetes-templating-with-helm/helm.exec.local.sh
+++ 03-Kubernetes-templating-with-helm/helm.exec.local.sh
@@ -1,3 +1,6 @@
 helm del $(helm ls --all --short) --purge
 helm install aws-checker --name aws-checker --wait
 kubectl get svc load-balancer
+
+image: devtest01registry01.azurecr.io/swagger_server:latest
+image: igorvoitov/swagger_server:latest
